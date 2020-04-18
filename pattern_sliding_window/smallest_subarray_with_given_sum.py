6c104dbbd32cc66965fa8203740aebe2f0bc8861
Deeksha <deeksha.aneja@izenda.com>
2020-04-19 02:25:42 +0530

    updated

--- pattern_sliding_window/smallest_subarray_with_given_sum.py
+++ pattern_sliding_window/smallest_subarray_with_given_sum.py
@@ -1,22 +1,16 @@
 import math
 
 def smallest_subarray_with_given_sum(s, arr):
-    window_start, window_end = 0, 0
+    window_start = 0
     window_sum = 0
-    min_window_len = math.inf
-    result = []
+    min_len = math.inf
     for window_end in range(len(arr)):
-        if window_sum < s:
-            window_sum += arr[window_end]
-        else:
-            window_sum = window_sum + arr[window_end]
-            min_window_len = min(min_window_len, window_end-window_start)
-            while window_sum - arr[window_start] >= s:
-                window_sum -= arr[window_start]
-                min_window_len = min(min_window_len, window_end-window_start)
-                window_start += 1
-    return min_window_len
-
+        window_sum += arr[window_end]
+        while window_sum >= s:
+            min_len = min(min_len, window_end - window_start + 1)
+            window_sum -= arr[window_start]
+            window_start += 1
+    return min_len
 
 def main():
     print("Smallest subarray length: " + str(smallest_subarray_with_given_sum(7, [2, 1, 5, 2, 3, 2])))
